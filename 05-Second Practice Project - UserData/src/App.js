1f32ee12e03d39401fee4a69a7068ea96247fee7
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-08-06 23:44:58 +0530

    managed states, added validation, added a user list, lifted up a state

--- 05-Second Practice Project - UserData/src/App.js
+++ 05-Second Practice Project - UserData/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import AddUser from './components/User/AddUser/AddUser';
+import React, { useState } from "react";
+import AddUser from "./components/User/AddUser/AddUser";
 // import Card from './components/UI/Card/Card';
+import UserList from "./components/User/UserList/UserList";
 
 function App() {
+
+  const [userDataArr, setUserDataArr] = useState([])
+
+  // const userDataArr = []
+  const onUserSubmit = (uName, uAge) => {
+    // userDataArr.push(userObj)
+    setUserDataArr(prevState => {
+      return [...prevState, {name : uName, age : uAge, id : Math.random().toString()}]
+    })
+  }
+  // console.log(userDataArr)
+
   return (
-    <AddUser />
+    <div>
+      <AddUser onAddingUser={onUserSubmit}/>
+      <UserList users={userDataArr}/>
+    </div>
   );
 }
 
--- 05-Second Practice Project - UserData/src/components/User/AddUser/AddUser.js
+++ 05-Second Practice Project - UserData/src/components/User/AddUser/AddUser.js
@@ -3,32 +3,55 @@ import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
 import { useState } from "react";
 
-const AddUser = () => {
+const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
-  const [enteredUserAge, setEnteredUserAge] = useState(0);
+  const [enteredUserAge, setEnteredUserAge] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log("Form is submitted");
+    if( enteredUserName.trim().length === 0 && enteredUserAge.trim().length === 0){
+      return
+    }
+    if(+enteredUserAge < 1) {
+      return
+    }
+    // console.log("Form is submitted");
     console.log(enteredUserName, enteredUserAge);
+    // const userArray = 
+    //   {
+    //     userName : enteredUserName,
+    //     userAge : +enteredUserAge
+    //   }
+
+      props.onAddingUser(enteredUserName, enteredUserAge)
+    setEnteredUserAge("");
+    setEnteredUserName("");
   };
 
   const userNameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
   };
-  
+
   const ageChangeHandler = (event) => {
     setEnteredUserAge(event.target.value);
   };
 
   return (
-    <Card cssClass={styles.input}>
+    <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
-        <input type="text" onChange={userNameChangeHandler} />
+        <input
+          type="text"
+          value={enteredUserName}
+          onChange={userNameChangeHandler}
+        />
 
         <label htmlFor="age">Age(years)</label>
-        <input type="number" onChange={ageChangeHandler} />
+        <input
+          type="number"
+          value={enteredUserAge}
+          onChange={ageChangeHandler}
+        />
 
         <Button type="submit">Add User</Button>
       </form>
