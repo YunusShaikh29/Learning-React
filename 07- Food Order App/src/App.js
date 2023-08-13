cc5845e713afd279553f0d1733b14430f72a2830
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-08-13 21:47:55 +0530

    managed the state to show or hide the modal

--- 07- Food Order App/src/App.js
+++ 07- Food Order App/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+
+  const [cardIsShow, setCardIsShown] = useState(false)
+
+  const cartShowHandler = () => {
+    setCardIsShown(true)
+  }
+
+  const cartHideHandler = () => {
+    setCardIsShown(false)
+  }
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cardIsShow && <Cart onHideCart={cartHideHandler}/>}
+      <Header onShowCart={cartShowHandler}/>
       <main>
         <Meals />
       </main>
