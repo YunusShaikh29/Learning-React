4168092629ca78224dbfcd977222b946daa67a74
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-08-14 20:36:21 +0530

    Adding a cart context

--- 07- Food Order App/src/App.js
+++ 07- Food Order App/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
 
 function App() {
 
@@ -16,13 +18,13 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cardIsShow && <Cart onHideCart={cartHideHandler}/>}
       <Header onShowCart={cartShowHandler}/>
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- 07- Food Order App/src/components/Layout/HeaderCartButton.js
+++ 07- Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartProvider from "../../store/CartProvider";
 
 const HeaderCartButton = (props) => {
+    const cartCxt = useContext(CartProvider)
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
-      <span className={classes.icon}>
-        <CartIcon />
-      </span>
-      <span>Your Card</span>
-      <span className={classes.badge}>3</span>
-    </button>
+    <CartProvider>
+      <button className={classes.button} onClick={props.onShowCart}>
+        <span className={classes.icon}>
+          <CartIcon />
+        </span>
+        <span>Your Card</span>
+        <span className={classes.badge}>{cartCxt.totalProductAmount}</span>
+      </button>
+    </CartProvider>
   );
 };
 
