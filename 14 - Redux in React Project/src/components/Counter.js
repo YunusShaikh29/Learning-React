1d295979b83000f6512df15b81ead8f7379d333a
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-08-25 22:25:28 +0530

    added redux store file, added states for different actions

--- 14 - Redux in React Project/src/components/Counter.js
+++ 14 - Redux in React Project/src/components/Counter.js
@@ -1,12 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  const dispatch = useDispatch()
+  const counter = useSelector(state => state.counter);
+  const showORHide = useSelector(state => state.showCounter)
+
+  const incrementCounter = () => {
+    dispatch({ type : 'increment'})
+  }
+
+  const increaseBy5 = () => {
+    dispatch({type : 'increase', amount : 5})
+  }
+
+  const decrementCounter = () => {
+    dispatch({type : 'decrement'})
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({type : 'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showORHide && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementCounter}>Increment</button>
+        <button onClick={increaseBy5}>increaseBy 5</button>
+        <button onClick={decrementCounter}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
