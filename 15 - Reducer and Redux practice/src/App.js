2de2354631ce9e0880c75c380d997855a3dc090d
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-08-30 23:08:03 +0530

    sending cart data to the backend/firebase

--- 15 - Reducer and Redux practice/src/App.js
+++ 15 - Reducer and Redux practice/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -6,6 +7,14 @@ import { useSelector } from 'react-redux';
 function App() {
 
   const showCart = useSelector(state => state.ui.isCartVisible)
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+    fetch('https://react-reducer-ce23c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',{
+      method : 'PUT',
+      body : JSON.stringify(cart)
+    })
+  },[cart])
 
   return (
     <Layout>
