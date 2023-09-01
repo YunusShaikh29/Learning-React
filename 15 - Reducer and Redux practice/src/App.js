fd94c8faca1b6f0abdc00a176c96ad08251fff7b
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-09-01 21:03:18 +0530

    adding the fetching/async login in action creator function

--- 15 - Reducer and Redux practice/src/App.js
+++ 15 - Reducer and Redux practice/src/App.js
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { uiActions } from "./store/ui-slice";
-
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let initial = true;
 
@@ -17,49 +16,57 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending the cart data",
-        })
-      );
-      const response = await fetch(
-        "https://react-reducer-ce23c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
+    // const sendCartData = async () => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "pending",
+    //       title: "Sending...",
+    //       message: "Sending the cart data",
+    //     })
+    //   );
+    //   const response = await fetch(
+    //     "https://react-reducer-ce23c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+    //     {
+    //       method: "PUT",
+    //       body: JSON.stringify(cart),
+    //     }
+    //   );
 
-      if (!response.ok) {
-        throw new Error("Request failed");
-      }
+    //   if (!response.ok) {
+    //     throw new Error("Request failed");
+    //   }
 
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent the cart data successfully! 🎉",
-        })
-      );
-    };
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "success",
+    //       title: "Success!",
+    //       message: "Sent the cart data successfully! 🎉",
+    //     })
+    //   );
+    // };
+
+    // if(initial){
+    //   initial = false
+    //   return
+    // }
+
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "error",
+    //       title: "Error",
+    //       message: error.message,
+    //     })
+    //   );
+    // });
 
     if(initial){
       initial = false
       return
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: error.message,
-        })
-      );
-    });
+    dispatch(sendCartData(cart))
+
   }, [cart, dispatch]);
 
   return (
--- 15 - Reducer and Redux practice/src/store/cart-slice.js
+++ 15 - Reducer and Redux practice/src/store/cart-slice.js
@@ -1,45 +1,93 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
-    name : 'cartSlice',
-    initialState : {
-        items : [],
-        totalQuantity : 0,
-        // totalPriceAmount : 0
+  name: "cartSlice",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    // totalPriceAmount : 0
+  },
+  reducers: {
+    addCartItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
     },
-    reducers : {
-        addCartItem(state, action) {
-            const newItem = action.payload;
-            const existingItem = state.items.find(item => item.id === newItem.id)
-            state.totalQuantity++;
-            if(!existingItem){
-                state.items.push({
-                    id : newItem.id,
-                    price : newItem.price,
-                    quantity : 1,
-                    totalPrice : newItem.price,
-                    name : newItem.title
-                })
-            }else {
-                existingItem.quantity++;
-                existingItem.totalPrice = existingItem.totalPrice + newItem.price
-            }
-        },
-        removeItem(state,action){
-            const id = action.payload;
-            const existingItem = state.items.find(item => item.id === id)
-            state.totalQuantity--;
-            if(existingItem.quantity === 1){
-                state.items = state.items.filter(item => item.id !== id)
-            }else{
-                existingItem.quantity--;
-                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
-            }
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~ ACTION CREATOR FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending the cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-reducer-ce23c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
         }
+      );
+
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent the cart data successfully! 🎉",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: error.message,
+        })
+      );
     }
-})
+  };
+};
 
-export const cartActions = cartSlice.actions
+export const cartActions = cartSlice.actions;
 
-export default cartSlice
+export default cartSlice;
