516952d74c40a153683cfe5188e3905a42e01930
Yunus Shaikh <yunusshaikh29082@gmail.com>
2023-10-17 21:19:23 +0530

    testing user interaction & state change

--- 25 - Unit Testing/src/components/Greeting.test.js
+++ 25 - Unit Testing/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 // test('Renders Hello World as a text', () => {
 //     // Arrange
@@ -34,4 +35,29 @@ describe("Greeting Component", () => {
     const outputElement = screen.getByText("good to see you", { exact: false });
     expect(outputElement).toBeInTheDocument();
   });
+
+  test('renders "changed text" if the button is Clicked', () => {
+    render(<Greeting />)
+
+    //Act
+    const buttonElement = screen.getByRole('button')
+    userEvent.click(buttonElement)
+
+    //Assert
+    const outputElement = screen.getByText("changed text", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  })
+
+  test('does not render "good to see you" text if the button was Clicked', () => {
+    render(<Greeting />)
+
+    //Act
+    const buttonElement = screen.getByRole('button')
+    userEvent.click(buttonElement)
+
+    //Assert
+    const outputElement = screen.queryByText("good to see you", { exact: false });
+    expect(outputElement).toBeNull()
+  })
+
 });
